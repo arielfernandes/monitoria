be1dcea2797c775ac859e154f02a4a7928e8d924
arielfernandes3 <0.arielfernandes.0@gmail.com>
2022-03-15 17:53:15 -0300

    matrizTransposta

---
+++ monitoriaCPP/loop/cadastro.cpp
@@ -0,0 +1,24 @@
+/*
+ Faça um programa em C/C++ para ler o nome, a idade e o sexo (M/F) de N pessoas, onde N deve ser lido no início do programa. 
+Nota: o sexo deverá receber apenas M ou F.
+Gere os seguintes relatórios:
+- Geral (com todos os dados);
+- Das mulheres;
+- Dos homens;
+- Das pessoas com idade >= 18;
+Além disso apresente:
+- O percentual de homens;
+- O percentual de mulheres;
+- A maior idade e o(s) respectivo(s) nome e idade;
+- A menor idade e o(s) respectivo(s) nome e idade.
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Quantos pessoas deseja cadastras? : ";
+    cin >> n;
+    return 0;
+}
